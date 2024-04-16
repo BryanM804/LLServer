c88463f7b9fb3278599a7a05e59a10ca56bcf47a
BryanM804 <bmulholland41@gmail.com>
2024-04-15 23:06:38 -0400

    removed setnumbers

--- src/logSet.js
+++ src/logSet.js
@@ -8,7 +8,7 @@ module.exports = (connection, setData) => {
         connection.connect((err) => {
             if (err) console.log(`Connection error: ${err}`);
 
-            connection.query(`INSERT INTO lifts (userID, movement, weight, reps, date, setnumber, settotal) VALUES('${setData.userid}', '${setData.movement}', ${setData.weight}, ${setData.reps}, '${setData.date}', 100000, ${setTotal});`, (err2, result) => {
+            connection.query(`INSERT INTO lifts (userID, movement, weight, reps, date, settotal, dateval) VALUES('${setData.userid}', '${setData.movement}', ${setData.weight}, ${setData.reps}, '${setData.date}', ${setTotal}, ${Date.parse(setData.date)});`, (err2, result) => {
                 if (err2) console.log(`Query error: ${err2}`);
 
                 console.log(`[${setData.date}] User ${setData.userid} Logged ${setData.movement} ${setData.weight}lbs x ${setData.reps} reps for ${setData.sets} sets.`);
