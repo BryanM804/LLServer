47ff89ab6fea8aa465ce0c5c1626bb4a51b2cbb6
BryanM804 <bmulholland41@gmail.com>
2024-04-16 23:52:18 -0400

    time logging

--- src/logSet.js
+++ src/logSet.js
@@ -8,7 +8,8 @@ module.exports = (connection, setData) => {
         connection.connect((err) => {
             if (err) console.log(`Connection error: ${err}`);
 
-            connection.query(`INSERT INTO lifts (userID, movement, weight, reps, date, settotal, dateval) VALUES('${setData.userid}', '${setData.movement}', ${setData.weight}, ${setData.reps}, '${setData.date}', ${setTotal}, ${Date.parse(setData.date)});`, (err2, result) => {
+            connection.query(`INSERT INTO lifts (userID, movement, weight, reps, date, settotal, dateval, time) 
+                    VALUES('${setData.userid}', '${setData.movement}', ${setData.weight}, ${setData.reps}, '${setData.date}', ${setTotal}, ${Date.parse(setData.date)}, '${new Date().toLocaleTimeString("en-US")}');`, (err2, result) => {
                 if (err2) console.log(`Query error: ${err2}`);
 
                 console.log(`[${setData.date}] User ${setData.userid} Logged ${setData.movement} ${setData.weight}lbs x ${setData.reps} reps for ${setData.sets} sets.`);
